d11658fecd428399b942c6bfdd080cec410c8893
TruongThinh2604 <163129860+TruongThinh2604@users.noreply.github.com>
2024-04-02 15:04:41 +0700

    Add files via upload

---
+++ MyClient.java
@@ -0,0 +1,23 @@
+package client;
+import java.net.*;  
+import java.io.*;  
+class MyClient{  
+public static void main(String args[])throws Exception{  
+Socket s=new Socket("localhost",2588);  
+DataInputStream din=new DataInputStream(s.getInputStream());  
+DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+  
+String str="",str2="";  
+while(!str.equals("stop")){  
+str=br.readLine();  
+dout.writeUTF(str);  
+dout.flush();  
+str2=din.readUTF();  
+System.out.println("bạn nói: "+str2);  
+}  
+  
+dout.close();  
+s.close();  
+}}  
+
